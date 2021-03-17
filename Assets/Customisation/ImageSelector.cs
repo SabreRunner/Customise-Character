fbef8246178e58d5475a6ca998fec292555fd8c6
SabreRunner <%Z*!3Mg!6kSUbE5EjRXMum4dNHq8s#yc>
2021-03-17 12:19:14 +0200

    Added image selector.

---
+++ Assets/Customisation/ImageSelector.cs
@@ -0,0 +1,32 @@
+using System;
+using PushForward.ExtensionMethods;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageSelector : BaseMonoBehaviour
+{
+    [SerializeField] Image image;
+    [SerializeField] Sprite[] images;
+
+    public void SetImage(int imageNumber)
+    {
+        if (!imageNumber.Between(0, this.images.Length - 1))
+        {
+            this.Log("SetImage", "Value of " + nameof(imageNumber) + " is out bounds.");
+            return;
+        }
+
+        if (this.images[imageNumber] == default)
+        {
+            this.Log("SetImage", "Images array does not contain a sprite at position: " + imageNumber);
+            return;
+        }
+        this.image.sprite = this.images[imageNumber];
+    }
+
+    private void OnValidate()
+    {
+        if (this.image == null)
+        { this.image = this.GetComponent<Image>(); }
+    }
+}
