d787aa4c9169f298333e52b097f60dc1919fbd74
SabreRunner <%Z*!3Mg!6kSUbE5EjRXMum4dNHq8s#yc>
2021-03-21 13:11:12 +0200

    Added connection to level and coins update.

--- Assets/Customisation/GameManager.cs
+++ Assets/Customisation/GameManager.cs
@@ -1,11 +1,33 @@
 namespace Customisation
 {
-    public class GameManager : BaseMonoBehaviour
+    using PushForward.EventSystem;
+    using UnityEngine;
+
+    public class GameManager : SingletonBehaviour<GameManager>
     {
         private static UserData userData;
 
-        public static int UserLevel => GameManager.userData.Level;
-        public static int UserCoins => GameManager.userData.Coins;
+        [SerializeField] private GameEventString levelUpdateEvent;
+        [SerializeField] private GameEventString coinsUpdateEvent;
+
+        public static int UserLevel
+        {
+            get => GameManager.userData.Level;
+            set
+            {
+                GameManager.userData.Level = value;
+                GameManager.Instance.levelUpdateEvent.Raise(value.ToString());
+            }
+        }
+        public static int UserCoins
+        {
+            get => GameManager.userData.Coins;
+            set
+            {
+                GameManager.userData.Coins = value;
+                GameManager.Instance.coinsUpdateEvent.Raise(value.ToString());
+            }
+        }
 
         private void Start()
         {
